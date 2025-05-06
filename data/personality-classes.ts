0e85fb606697e3f59713ab3fd5a7ec4fd7ef525b
Mahamudun nabi siam <mahamudunn1005@gmail.com>
2025-05-06 19:02:11 +0600

    Update personality-classes.ts
    Bangla dubbed

--- data/personality-classes.ts
+++ data/personality-classes.ts
@@ -3,34 +3,34 @@ import { PersonalityClass } from "../lib/personality-test";
 export const personalityClasses: PersonalityClass[] = [
   {
     type: "E",
-    description: "Extroverted",
+    description: "Extroverted - কথা বলতে পছন্দ করে",
   },
   {
     type: "I",
-    description: "Introverted",
+    description: "Introverted - চুপচাপ থাকতে পছন্দ করে",
   },
   {
     type: "S",
-    description: "Sensing",
+    description: "Sensing - বাস্তব কিছুতে বিশ্বাস করে",
   },
   {
     type: "N",
-    description: "Intuitive",
+    description: "Intuitive - কল্পনা করতে ভালোবাসে",
   },
   {
     type: "T",
-    description: "Thinking",
+    description: "Thinking - যুক্তি দিয়ে চিন্তা করে",
   },
   {
     type: "F",
-    description: "Feeling",
+    description: "Feeling - মন দিয়ে চিন্তা করে",
   },
   {
     type: "P",
-    description: "Perceiving",
+    description: "Perceiving - সময় নিয়ে চলতে চায়",
   },
   {
     type: "J",
-    description: "Judging",
+    description: "Judging - নিয়ম মেনে চলতে চায়",
   },
 ];
